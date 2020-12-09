3bfa825a2688885309f965c344a3455eba16f4bd
olapuente <o.lapuente.santana@tue.nl>
2020-12-09 12:38:09 +0100

    First copy of local folder

---
+++ examples/workflow_analysis.R
@@ -0,0 +1,107 @@
+# ---------------------------------------------------------------------------
+# Install Estimate Systems Immune Response (easier) package
+# ---------------------------------------------------------------------------
+install.packages("~/ownCloud/SystemsImmunoOncology/Mechanistic_signatures_project/easier_1.0.3.tar.gz", repos = NULL, type="source")
+library(easier)
+
+# ******************
+# Example: Riaz
+# ******************
+data("Riaz_data")
+
+# ---------------------------------------------------------------------------
+# Computation of cell fractions
+# ---------------------------------------------------------------------------
+cell_fractions <- compute_cell_fractions(RNA.tpm = Riaz_data$tpm_RNAseq)
+
+# ---------------------------------------------------------------------------
+# Computation of pathways scores
+# ---------------------------------------------------------------------------
+Pathway_activities <- compute_pathways_scores(RNA.counts = Riaz_data$raw_counts_RNAseq,
+                                              remove.genes.ICB_proxies = TRUE)
+
+# ---------------------------------------------------------------------------
+# Computation of TF activity
+# ---------------------------------------------------------------------------
+TF_activities <- compute_TF_activity(RNA.tpm = Riaz_data$tpm_RNAseq,
+                                     remove.genes.ICB_proxies = FALSE)
+
+# ---------------------------------------------------------------------------
+# Computation of LR pairs weights
+# ---------------------------------------------------------------------------
+LR_data <- compute_LR_pairs(RNA.tpm = Riaz_data$tpm_RNAseq,
+                            remove.genes.ICB_proxies = FALSE,
+                            compute.cytokines.pairs = FALSE,
+                            cancertype = cancertype)
+
+# ---------------------------------------------------------------------------
+# Computation of cell-cell interactions scores
+# ---------------------------------------------------------------------------
+CCpairs_grouped <- compute_CC_pairs_grouped(lrpairs = LR_data$LRpairs,
+                                            cancertype = cancertype)
+
+# ---------------------------------------------------------------------------
+# Predict immune response
+# ---------------------------------------------------------------------------
+
+# *********************
+# Collect views data
+pathways_scores <- Pathway_activities$scores
+cell_fractions <- cell_fractions
+lrpairs_weights <- LR_data$LRpairs
+tfs_scores <- TF_activities$scores
+ccpairsgrouped_scores <- CCpairs_grouped$score
+
+# *********************
+# Specify cancer type
+cancertype <- "SKCM"
+
+predictions_immune_response <- predict_immune_response(pathways = pathways_scores,
+                                                       immunecells = cell_fractions,
+                                                       lrpairs = lrpairs_weights,
+                                                       tfs = tfs_scores,
+                                                       ccpairsgroupedscores = ccpairsgrouped_scores,
+                                                       cancertype = cancertype)
+
+# ---------------------------------------------------------------------------
+# Compare predicted immune response with real patient response
+# Assess likelihood of patient response to ICB therapy
+# ---------------------------------------------------------------------------
+
+# *********************
+# Provide tpm data (row: genes ; columns: samples)
+tpm <- Riaz_data$tpm_RNAseq
+
+# *********************
+# Patient labels (factor variable with two levels: NR and R)
+patient_labels <- Riaz_data$patient_response
+
+# *********************
+# Specify output file path
+output_file_path <- "/Users/Oscar/Desktop/Riaz"
+
+compare_immune_response(predictions_immune_response = predictions_immune_response,
+                        real_patient_response = patient_labels,
+                        RNA.tpm = tpm,
+                        output_file_path = output_file_path,
+                        cancertype = cancertype)
+
+
+# ---------------------------------------------------------------------------
+# Have a look into the biomarkers
+# ---------------------------------------------------------------------------
+
+explore_biomarkers(pathways = pathways_scores,
+                   immunecells = cell_fractions,
+                   # lrpairs = lrpairs_weights,
+                   # tfs = tfs_scores,
+                   # ccpairsgroupedscores = ccpairsgrouped_scores,
+                   cancertype = cancertype,
+                   real_patient_response = patient_labels,
+                   output_file_path = "/Users/Oscar/Desktop/Riaz")
+
+
+
+
+
+
